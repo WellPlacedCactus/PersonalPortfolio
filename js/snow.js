525d3b17308afd964e8e97448b2501a5a31a06ce
WellPlacedCactus <42874619+WellPlacedCactus@users.noreply.github.com>
2021-10-08 17:56:05 -0500

    Added information to pages

--- js/snow.js
+++
@@ -1,8 +0,0 @@
-
-document.onload = function() {
-	const canvas = document.querySelector('canvas');
-	const c = canvas.getContext('2d');
-	c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-	c.fillRect(0, 0, canvas.width * 0.15 / 2, canvas.height);
-	c.fillRect(canvas.width * (0.85 + 0.15 / 2), 0, canvas.width * 0.15 / 2, canvas.height);
-};
